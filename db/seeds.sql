5a4842f8e1f5cb1cfe6902c3d77b35ddc2f66c2f
Vickee Shulman <victoriashulman25@gmail.com>
2021-03-05 16:58:45 -0500

    file changes with updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburglar', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Praise Cheezus', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('James Buffet', false);
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require('../config/orm.js');
+
+const burger = {
+  all(cb) {
+    orm.selectAll('burgers', (res) => cb(res));
+  },
+  // The variables cols and vals are arrays.
+  create(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, (res) => cb(res));
+  },
+  update(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.deleteOne('burgers', condition, (res) => cb(res));
+  },
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
